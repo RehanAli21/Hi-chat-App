070acba0ade5112615b8e3bedebb81fdb601855f
Rehan Ali <rehan.shah70@yahoo.com>
2021-01-27 17:03:20 +0500

    FriendContext removed and UserContext Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { UserProvider } from './Components/AppSubComps/UserContext'
 import Login from './Components/Login'
 import Signup from './Components/SignUp'
 import MainApp from './Components/App'
@@ -28,16 +29,18 @@ class App extends Component {
 	render() {
 		return (
 			<Router>
-				<Route
-					path='/'
-					exact
-					render={() => <Login changeTheme={this.changeTheme} />}
-				/>
-				<Route
-					path='/app'
-					exact
-					render={() => <MainApp theme={this.state.theme} />}
-				/>
+				<UserProvider>
+					<Route
+						path='/'
+						exact
+						render={() => <Login changeTheme={this.changeTheme} />}
+					/>
+					<Route
+						path='/app'
+						exact
+						render={() => <MainApp theme={this.state.theme} />}
+					/>
+				</UserProvider>
 				<Route
 					path='/signup'
 					exact
--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FriendsProvider } from './AppSubComps/FriendsContext'
 import Nav from './AppSubComps/Nav'
 import Contacts from './AppSubComps/Contacts'
 import Msgs from './AppSubComps/Msgs'
@@ -11,10 +10,8 @@ const App = ({ theme }) => {
 		<div>
 			<Nav t={theme} />
 			<main>
-				<FriendsProvider>
-					<Contacts id={id} />
-					<Msgs id={id} />
-				</FriendsProvider>
+				<Contacts id={id} />
+				<Msgs id={id} />
 			</main>
 		</div>
 	)
